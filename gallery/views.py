b607859cfaecf87aa0b9d826c2b80ef899243409
Mutukukioko <kharrylungu@gmail.com>
2023-08-17 11:29:17 +0300

    No form just random  regular images

--- gallery/views.py
+++ gallery/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 import requests
 
 def image_gallery(request):
+    selected_category = request.GET.get('category', 'nature')  
     # Fetch images from Unsplash API
     # Replace 'YOUR_ACCESS_KEY' with your actual Unsplash API access key
-    response = requests.get('https://api.unsplash.com/photos/?client_id=OeBTDjutwsJi5BC0gy1UAfl4i4lhLHYuqDJgvbcdciM&query={selected_category}')
+    response = requests.get(f'https://api.unsplash.com/photos/?client_id=YOUR-ACCESSS-KEY&query={selected_category}')
     images = response.json()
-
+    print(response.content)
     context = {'images': images}
     return render(request, 'gallery/image_gallery.html', context)
